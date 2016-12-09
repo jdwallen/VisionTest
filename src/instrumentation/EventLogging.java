776a0a05b473a286ca35bbc85d9b1e5ea5bccbc1
Jon Wallen <jdwallen@hotmail.com>
2016-12-09 11:58:41 -0800

    Logging Updates

--- src/instrumentation/EventLogging.java
+++ src/instrumentation/EventLogging.java
@@ -6,14 +6,15 @@
 
 package instrumentation;
 
-//import edu.wpi.first.wpilibj.
-
 import java.util.Date;
 import java.util.*;
-//import java.util.*;
 import java.text.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
-public class EventLog {
+public class EventLogging extends Instrumentation {
 	
 	// NORMAL events document "normal" execution events, e.g, the beginning and end of major functional
 	// flows.
@@ -51,55 +52,108 @@ public class EventLog {
 						TBD
 	};
 
+	private boolean logAvailable = false;
+
+	public EventLogging () {
+
+		// Create the directory to hold the events file.
+		super();
+		logAvailable = instrumentationAvailable();
+
+	}
+
 	// Define the event log.  Event are stored locally to save throughput.  The
 	// saveEventLog can be invoked (say, after a match is over) to dump the log
 	// to the Driver Station or storage device.
-	private static List<String> eventLog = new ArrayList<String>();
-	
+	private List<String> eventLog = new ArrayList<String>(10000);
+
 	// Format string for time and date.
-	private final static SimpleDateFormat dateFormat = new SimpleDateFormat ("yyyy.MM.dd hh:mm:ss.ssssss");		
-	
+	private final SimpleDateFormat dateFormat = new SimpleDateFormat ("yyyy.MM.dd hh:mm:ss.ssssss");		
+
+	boolean loggingAvailable() {
+		return logAvailable;
+	}
+
 	// This method logs the specified event onto the eventLog.
-	private static void logEvent (String event) {
-		  
+	private void logEvent (String event) {
+
 		Date date       = new Date();
 		String eventStr = dateFormat.format(date) + ' ' + event;
 		eventLog.add(eventStr);
-		
+
 		System.out.println(eventStr);  // Parameterize this?
 
 	}
-	
+
 	// This method logs a NORMAL event.
-	public static void logNormalEvent (NORMALEVENTS event, String auxData) {
+	public void logNormalEvent (NORMALEVENTS event, String auxData) {
 
-		logEvent ("NORMAL      " + event.name() + " " + auxData);
+		if (loggingAvailable()) logEvent ("NORMAL      " + event.name() + " " + auxData);
 
 	}
-	
+
 	// This method logs an INTERESTING event.
-	public static void logInterestingEvent (INTERESTINGEVENTS event, String auxData) {
+	public void logInterestingEvent (INTERESTINGEVENTS event, String auxData) {
 
-		logEvent ("INTERESTING " + event.name() + " " + auxData);
+		if (loggingAvailable()) logEvent ("INTERESTING " + event.name() + " " + auxData);
 
 	}
-	
+
 	// This method logs a BAD event.
-	public static void logBadEvent (BADEVENTS event, String auxData) {
+	public void logBadEvent (BADEVENTS event, String auxData) {
 
-		logEvent ("BAD         " + event.name() + " " + auxData);
+		if (loggingAvailable()) logEvent ("BAD         " + event.name() + " " + auxData);
 
 	}
-	
+
 	// This method dumps the event log to TBD.
-	public static void saveEventLog () {
-		
-		// Write the event log someplace.
-		for (String element : eventLog) {
-		    System.out.println(element);  // Save to Networktables
+	public void saveEventLog () {
+
+		BufferedWriter bw;
+		FileWriter     fw;
+
+		if (loggingAvailable()) {
+
+			// Create a File object with the event log file name.
+			File logFile = new File(dataDirectoryName() + "\\Events.txt");
+
+			// if it happens to already exist, delete it.
+			if (logFile.exists()) logFile.delete();
+
+			try {
+
+				// if we can create the file
+				if (logFile.createNewFile()) {
+
+					// Try to dump it to the file.
+					fw = new FileWriter(logFile);
+					bw = new BufferedWriter(fw);
+
+					// Write the event log someplace.
+					for (String element : eventLog) {
+
+						// On the roboRIO, users should store files to /home/lvuser or subfolders created there. Or,
+						// write them to a USB thumbstick at /media/sda1 (or reference /U/ and /V/).  USB stick may have
+						// to be FAT-formatted and not "too large".
+						bw.write(element);
+						bw.write("\r\n");
+						System.out.println(element); 
+					}
+
+					bw.close();
+					fw.close();
+				}
+			}
+			catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+
 		}
-		
+
 		// Free up memory.
 		eventLog.clear();
 	}
 }
+
--- src/instrumentation/ImageLogging.java
+++ src/instrumentation/ImageLogging.java
@@ -5,34 +5,86 @@
 
 package instrumentation;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.opencv.core.Mat;
 import org.opencv.imgcodecs.Imgcodecs;
 
-public class ImageLogging {
+public class ImageLogging extends Instrumentation {
 
+	private boolean logAvailable = false;
+	private final int MAXLISTENTRIES = 10000;
+	
+	public ImageLogging (){
+		super();
+	}
+	
 	// Define the list of images to save.  Images are stored locally to save throughput.  The
 	// saveImages can be invoked (say, after a match is over) to dump the images
-	// to the Driver Station or storage device.
-	private static List<Mat> imageList = new ArrayList<Mat>(10000);
+	// to a storage device.
+	private List<Mat>     imageList     = new ArrayList<Mat>(MAXLISTENTRIES);
+	
+	// Define a list of times at which the image was created.  These are used in the creation
+	// of the file name when the image is written.  This time can assist in correlating the
+	// image to events in the event log.
+	private List<String>  timeList      = new ArrayList<String>(MAXLISTENTRIES);
+	
+	// Define a list of contour IDs.  These are used in the creation
+	// of the file name when the image is written.  This contour ID can assist in correlating the
+	// image to events in the event log.
+	private List<Integer> contourIDList = new ArrayList<Integer>(MAXLISTENTRIES);
+	
+	boolean loggingAvailable() {
+		return logAvailable;
+	}
 	
 	// Save an image to the imageList.
-	public static void save (Mat image) {
+	public void save (Mat image, int contourID) {
+		
+		SimpleDateFormat dateFormat = new SimpleDateFormat ("hh.mm.ssssss");
+		Date date       = new Date();
 		imageList.add(image);
+		contourIDList.add(contourID);
+		timeList.add(dateFormat.format(date));
 	}
 	
 	// This method dumps the imageList to TBD.
-	public static void saveImages () {
+	public void saveImageLog () {
 		
-		// Write the event log someplace.
-		for (Mat element : imageList) {
-		    Imgcodecs.imwrite("TBD", element);  // Save to Networktables?
+		// On the roboRIO, users should store files to /home/lvuser or subfolders created there. Or,
+		// write them to a USB thumbstick at /media/sda1 (or reference /U/ and /V/).  USB stick may have
+		// to be FAT-formatted and not "too large".
+
+		if (loggingAvailable()) {
+
+			// Write the event log someplace.
+			for (int i = 0; i < imageList.size(); i++) {
+				Imgcodecs.imwrite(dataDirectoryName() + "\\" + i + " " +timeList.get(i) + " " + "C" + contourIDList.get(i) + ".jpg",
+						imageList.get(i));
+			}
+
+			// Free up memory.
+			imageList.clear();
+			contourIDList.clear();
+			timeList.clear();
+
 		}
-		
-		// Free up memory.
-		imageList.clear();
 
 	}
+	
+//	File input = new File("C:\\File\\1.tif");
+//
+//	BufferedImage image = ImageIO.read(input);
+//	// Here we convert into *supported* format
+//	BufferedImage imageCopy =
+//	    new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_3BYTE_BGR);
+//	imageCopy.getGraphics().drawImage(image, 0, 0, null);
+//
+//	byte[] data = ((DataBufferByte) imageCopy.getRaster().getDataBuffer()).getData();  
+//	Mat img = new Mat(image.getHeight(),image.getWidth(), CvType.CV_8UC3);
+//	img.put(0, 0, data);           
+//	Imgcodecs.imwrite("C:\\File\\input.jpg", img);
 }
